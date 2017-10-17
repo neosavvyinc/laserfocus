4bf0bfbd3a06bc78809c099a5b545a1a9118ac5e
Adam Parrish <aparrish@neosavvy.com>
2017-10-17 12:23:36 -0400

    Initial Page Layout
    - Toolbar Layout Implementation

---
+++ pages/components/KCGestationBoard.js
@@ -0,0 +1,15 @@
+export default () => (
+    <div className="board">
+
+        <style jsx>{`
+        .board {
+            margin: auto;
+            display: flex;
+            width: 1375px;
+            height: 674px;
+        }
+
+        `}
+    </style>
+    </div>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,29 @@
 import Page from '../layouts/main'
 import KCHeader from './components/KCHeader';
+import KCToolbar from './components/KCToolbar';
+import KCGestationBoard from './components/KCGestationBoard';
 
 export default () => (
     <Page>
          <div className="app">
 
             <KCHeader/>
+            <KCToolbar/>
+            <KCGestationBoard/>
 
-             <style jsx>{`
 
-                .app {
-                    margin: auto;
-                    width: 1440px;
-                    height: 824px;
-                    background-color: #eff3f6;
-                }
+            <style jsx>{`
 
-                `}</style>
+               .app {
+                   margin: auto;
+                   width: 1440px;
+                   height: 824px;
+                   background-color: #eff3f6;
+               }
+
+
+            `}
+            </style>
 
 
          </div>
