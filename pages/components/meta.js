0aa688218afba32b05583e99511b8f6d07dc71f9
Chris Mathers <xee@xemedia.com>
2017-10-20 00:02:58 +0100

    Set some initial body and header styles to tighten up margins.

--- pages/components/meta.js
+++ pages/components/meta.js
@@ -6,35 +6,40 @@ export default () => (
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta charSet="utf-8" />
             <style jsx global>{`
-                    .text-style-emphasis {
-                        font-family: 'Source Sans Pro';
-                        font-size: 16px;
-                        font-weight: 600;
-                        text-align: left;
-                        color: #303a71;
-                    }
 
-                    .linkText {
-                        font-family: 'Source Sans Pro';
-                        font-size: 15px;
-                        font-weight: 600;
-                        text-align: left;
-                        color: #00aaff;
-                    }
+                body {
+                    margin: 0;
+                }
 
-                    .standardText {
-                        font-family: 'Source Sans Pro';
-                        font-size: 15px;
-                        text-align: left;
-                        color: #354052;
-                    }
+                .text-style-emphasis {
+                    font-family: 'Source Sans Pro';
+                    font-size: 16px;
+                    font-weight: 600;
+                    text-align: left;
+                    color: #303a71;
+                }
 
-                    .text-style-light {
-                        font-family: 'Source Sans Pro';
-                        font-size: 14px;
-                        text-align: left;
-                        color: #9b9b9b;
-                    }
+                .linkText {
+                    font-family: 'Source Sans Pro';
+                    font-size: 15px;
+                    font-weight: 600;
+                    text-align: left;
+                    color: #00aaff;
+                }
+
+                .standardText {
+                    font-family: 'Source Sans Pro';
+                    font-size: 15px;
+                    text-align: left;
+                    color: #354052;
+                }
+
+                .text-style-light {
+                    font-family: 'Source Sans Pro';
+                    font-size: 14px;
+                    text-align: left;
+                    color: #9b9b9b;
+                }
             `}</style>
         </Head>
 )
